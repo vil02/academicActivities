b79279b9788cca40a42279f2a5b4c6f90574cd5e
piotr.idzik <vil02@o2.pl>
2021-06-22 21:11:41 +0200

    added lock

--- ubuntu_local_build.sh
+++ ubuntu_local_build.sh
@@ -1,8 +1,59 @@
 #!/bin/bash
 
-readonly TEX_FILE="academic_activities_eng.tex"
+set -e
+set -u
 
-pdflatex -interaction=batchmode -draftmode $TEX_FILE
-pdflatex -interaction=batchmode -draftmode $TEX_FILE
-pdflatex -interaction=batchmode $TEX_FILE
+script_lock=$(dirname "${0}")/"build_lock_dir"
+readonly script_lock
+
+declare -r tex_file="academic_activities_eng.tex"
+
+function is_already_running()
+{
+    declare -i return_value=0
+    test -d "${script_lock}" ||
+    {
+        return_value=1
+    }
+    return "${return_value}"
+}
+ 
+ 
+function create_lock()
+{
+    mkdir "${script_lock}" ||
+    {
+        printf "Can not create lock\n"
+        exit 2
+    }
+}
+ 
+function remove_lock() 
+{
+    rm -rf "${script_lock}" ||
+    {
+        printf "Can not remove lock\n"
+        exit 3  
+    }
+}
+
+if is_already_running
+then
+    printf "Can not acquire lock (another instance is running?) - exiting.\n"
+    exit 1
+fi
+
+create_lock
+
+{
+    pdflatex -interaction=batchmode -draftmode $tex_file &&
+    pdflatex -interaction=batchmode -draftmode $tex_file &&
+    pdflatex -interaction=batchmode $tex_file
+} || {
+    remove_lock
+    printf "Error while building document\n"
+    exit 4
+}
+
+remove_lock
 
