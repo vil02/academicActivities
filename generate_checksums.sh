45752674d4b48af8c2962c3d891ca1f6178d78aa
piotr.idzik <vil02@o2.pl>
2021-08-29 17:21:43 +0200

    generate_checksums.sh added

---
+++ generate_checksums.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+script_lock=$(dirname "${0}")/"checksum_generator_lock_dir"
+readonly script_lock
+
+function is_already_running()
+{
+    declare -i return_value=0
+    test -d "${script_lock}" ||
+    {
+        return_value=1
+    }
+    return "${return_value}"
+}
+
+function create_lock()
+{
+    mkdir "${script_lock}" ||
+    {
+        printf "Cannot create lock\n"
+        exit 2
+    }
+}
+
+function remove_lock()
+{
+    rm -rf "${script_lock}" ||
+    {
+        printf "Cannot remove lock\n"
+        exit 3
+    }
+}
+
+if is_already_running
+then
+    printf "Cannot acquire lock (another instance is running?) - exiting.\n"
+    exit 1
+fi
+
+create_lock
+
+output_folder=$(./get_output_folder.sh) ||
+{
+    remove_lock
+    printf "Cannot get the result folder\n"
+    exit 5
+}
+readonly output_folder
+
+cd "$output_folder" ||
+{
+    remove_lock
+    printf "Cannot access result folder\n"
+    exit 6
+}
+
+for sum_t in "md5sum" "sha1sum" "sha224sum" "sha384sum" "sha256sum" "sha512sum"
+do
+    $sum_t ./*.pdf  > "checksum.""${sum_t::${#sum_t}-3}" ||
+    {
+        remove_lock
+        printf "Error while generating %s\n" $sum_t
+        exit 6
+    }
+done
+
+cd -
+
+remove_lock
